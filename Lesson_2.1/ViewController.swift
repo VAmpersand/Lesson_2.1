5cdb607689dda88ec98023bfcb574a49ef643d1d
Viktor <viktor.prikolota@exinity.com>
2024-02-14 17:35:48 -0300

    Initial commit

---
+++ Lesson_2.1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Lesson_2.1
+//
+//  Created by Viktor Prikolota on 14.02.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
